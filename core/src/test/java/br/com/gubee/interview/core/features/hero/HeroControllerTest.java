303b50551876cc19dbe8cd51c324820f351985f6
eltonk2k <elieltonpb1@gmail.com>
2023-07-14 11:38:12 -0300

    implementando testes unitarios

--- core/src/test/java/br/com/gubee/interview/core/features/hero/HeroControllerTest.java
+++ core/src/test/java/br/com/gubee/interview/core/features/hero/HeroControllerTest.java
@@ -1,11 +1,14 @@
 package br.com.gubee.interview.core.features.hero;
 
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.util.List;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
 
 @ExtendWith(SpringExtension.class)
 public class HeroControllerTest {
@@ -15,10 +18,19 @@ public class HeroControllerTest {
     private HeroController heroController;
 
 
+    @BeforeEach
+    public void setup(){
+        standaloneSetup(this.heroController);
+    }
 
     @Test
     public void test() {
-
+                given()
+                    .accept(ContentType.JSON)
+                .when()
+                    .get("/heros/{id}", 1L)//o objeto não e uma String
+                .then()
+                    .statusCode(HttpStatus.BAD_REQUEST.value());
 
     }
 }
